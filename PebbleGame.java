2e78c0340c04eeaa9b1ec69b353927fd0c3dd260
Louis Alexander <louisalexander2001@gmail.com>
2021-10-29 14:36:25 +0100

    Crude code for locating a pebble bag and refilling the bag added

--- PebbleGame.java
+++ PebbleGame.java
@@ -1,5 +1,47 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class PebbleGame {
-    ...
+
+    private List<Bag> bags = new ArrayList();
+
+
+    private Bag locateBag(bagName name){
+        Bag desiredBag = null;
+        for (Bag bag : this.bags){
+            if (bag.getName() == name){
+                desiredBag = bag;
+                break;
+            }
+        }
+        return desiredBag;
+    }
+
+    private synchronized void refillBag(Bag bag){
+        switch (bag.getName()){
+            case X:
+                Bag bagA = locateBag(bagName.A);
+                bag.setPebbles(bagA.getPebbles());
+                bagA.clearPebbles();
+                break;
+            case Y:
+                Bag bagB = locateBag(bagName.B);
+                bag.setPebbles(bagB.getPebbles());
+                bagB.clearPebbles();
+                break;
+            case Z:
+                Bag bagC = locateBag(bagName.C);
+                bag.setPebbles(bagC.getPebbles());
+                bagC.clearPebbles();
+                break;
+            case A: case B: case C: // this should never be true
+                break;
+            default:
+                break;
+        }
+
+    }
+
     class Player {
         ...
         public Player(String arg){
