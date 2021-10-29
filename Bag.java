52e6c5588be9ba63711b11984a714f8cd0f7ba08
Louis Alexander <louisalexander2001@gmail.com>
2021-10-29 14:38:45 +0100

    Crude code for bag setup created
    Crude code for name and contents getter and setter methods added. crude code for loading the contents of the bag from a file also added - inout validation needed.
    Synchronized method for getting a random pebble also added

--- Bag.java
+++ Bag.java
@@ -1,6 +1,71 @@
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.List;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.IOException;
+
+
+enum bagName {A, B, C, X, Y, Z}
+
 public class Bag {
-    ...
-    public Bag(String arg){
-        
+    bagName name;
+    private List<Integer> bagContents = new ArrayList();
+    Random rand = new Random();
+
+    public void setName(bagName name){
+        this.name = name;
+    }
+    public bagName getName(){
+        return this.name;
+    }
+
+    public Boolean isEmpty(){
+        if (this.bagContents.size() < 1){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public List<Integer> getPebbles(){
+        return this.bagContents;
+    }
+    public void setPebbles(List<Integer> pebbles){
+        this.bagContents = pebbles;
+    }
+    public void clearPebbles(){
+        this.bagContents.clear();
+    }
+
+    private void loadPebbles(String pebbleFile){
+        Path filePath = Paths.get(pebbleFile);
+        try(BufferedReader reader = Files.newBufferedReader(filePath, StandardCharsets.UTF_8)){
+            String line = reader.readLine();
+            while (line != null){
+                String[] pebbles = line.split(",");
+                for (String pebble : pebbles){
+                    this.bagContents.add(Integer.parseInt(pebble));
+                }
+                line = reader.readLine();
+            }
+        }catch (IOException IOException){
+            IOException.printStackTrace();
+        }
+    }
+
+    public synchronized Integer getRandomPebble(){
+        int randomInt = rand.nextInt(bagContents.size());
+        int pebble = bagContents.get(randomInt);
+        bagContents.remove(randomInt);
+        return pebble;
+    }
+
+    public Bag(bagName name, String pebbleFile){
+        setName(name);
+        loadPebbles(pebbleFile);
     }
 }
