9e21c32fde102c8eb28dcf73047297fc00372a78
Louis Alexander <louisalexander2001@gmail.com>
2021-10-30 01:15:02 +0100

    Did a couple hours work on the PebbleGame file working on both the main class and the Player class

--- PebbleGame.java
+++ PebbleGame.java
@@ -2,13 +2,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
 import java.util.Scanner;
-
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Random;
 
 public class PebbleGame{
 
     private Integer numOfPlayers;
     private List<Bag> bags = new ArrayList();
-
+    private List<Player> players = new ArrayList();
+    private List<Thread> threadPool;
 
     private Bag locateBag(bagName name){
         Bag desiredBag = null;
@@ -46,10 +50,21 @@ public class PebbleGame{
 
     }
 
+    public void safeQuit(){
+        for (Player player : this.players){
+            player.closeFile();
+        }
+        System.out.println("All files are now closed.\n Thank you for playing.");
+    }
+
     class Player implements Runnable{
         
         private Integer playerNumber;
         private List<Entry<Integer, bagName>> playersHand = new ArrayList<>();
+        private bagName previousPickBag;
+        private FileWriter writer;
+        Random random = new java.util.Random();
+
 
         private Boolean checkHand(){
             int total = 0;
@@ -63,14 +78,79 @@ public class PebbleGame{
             }
         }
         
-        public Player(String arg){
-
+        public void closeFile(){
+            try{
+                this.writer.close();
+            }catch (IOException e){
+                System.out.println("An IOException occurred.");
+                e.printStackTrace();
+            }
         }
 
-        public void run(){
-            
+        public void pickAPebble(){
+            Entry<Integer, bagName> pebble = bags.get(random.nextInt(3)).getRandomPebble();
+            this.playersHand.add(pebble);
+            this.previousPickBag = pebble.getValue();
+            try{
+                this.writer.write("Player" + this.playerNumber.toString() + " has drawn a " + pebble.getKey().toString() +
+                                  " from bag " + pebble.getValue().toString());
+                this.writer.write("Player" + this.playerNumber.toString() + "'s hand is " + this.playersHand.toString());
+            }catch (IOException e){
+                System.out.println("An IOException occurred.");
+                e.printStackTrace();
+            }
         }
-    }
+        public void discardAPebble(){
+            Integer randInt = random.nextInt(10);
+            Entry<Integer, bagName> pebble = this.playersHand.get(randInt);
+            this.playersHand.remove(randInt);
+            switch (this.previousPickBag){
+                case X:
+                    bags.get(0).addPebble(pebble);
+                case Y:
+                    bags.get(1).addPebble(pebble);
+                case Z:
+                    bags.get(2).addPebble(pebble);
+            }
+            try{
+                this.writer.write("Player" + this.playerNumber.toString() + " has discarded a " + pebble.getKey().toString() +
+                                  " to bag " + this.previousPickBag.toString());
+                this.writer.write("Player" + this.playerNumber.toString() + "'s hand is " + this.playersHand.toString());
+            }catch (IOException e){
+                System.out.println("An IOException occurred.");
+                e.printStackTrace();
+            }
+        }
+
+        public Player(Integer playerNumber){
+            this.playerNumber = playerNumber;
+            String fileName = "player"+playerNumber.toString()+"_output.txt";
+            File file = new File(fileName);
+
+
+            try{
+                if(file.exists() && !file.isDirectory()) { 
+                    this.writer = new FileWriter(fileName);
+                }else{
+                    file.createNewFile();
+                    this.writer = new FileWriter(fileName);
+                }
+            } catch (IOException e){
+                System.out.println("An IOException occurred.");
+                e.printStackTrace();
+            }
+
+            public void run(){
+                Boolean handStatus = false;
+                handStatus = checkHand();
+                while (!handStatus){
+                    discardAPebble();
+                    pickAPebble();
+                    handStatus = checkHand();
+                }
+                // here i tell everyone i have a hand
+            }
+        }
 
     public PebbleGame(){
         //setup scanner
@@ -84,15 +164,23 @@ public class PebbleGame{
         System.out.print("Please enter the number of players: "); 
         this.numOfPlayers = sc.nextInt();
         //create bags
-        bags.add(new Bag(bagName.A));
-        bags.add(new Bag(bagName.B));
-        bags.add(new Bag(bagName.C));
         System.out.print("Please enter the location of bag 0 to load: "); 
         bags.add(new Bag(bagName.X, sc.nextLine()));
         System.out.print("Please enter the location of bag 1 to load: "); 
         bags.add(new Bag(bagName.Y, sc.nextLine()));
         System.out.print("Please enter the location of bag 2 to load: "); 
         bags.add(new Bag(bagName.Z, sc.nextLine()));
+        bags.add(new Bag(bagName.A));
+        bags.add(new Bag(bagName.B));
+        bags.add(new Bag(bagName.C));
+        for (int i=0; i<this.numOfPlayers; i++){
+            this.players.add(new Player(i));
+            this.threadPool.add(new Thread(players.get(i)));
+        }
+        for (Thread thread : threadPool){
+            thread.start();
+        }
+        
     }
 
     public static void main(String[] args){
