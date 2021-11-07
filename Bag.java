8585ef27150df814269d479f4b6053cb01830fbe
Louis Alexander <louisalexander2001@gmail.com>
2021-11-07 18:52:33 +0000

    dev

--- Bag.java
+++ Bag.java
@@ -1,3 +1,5 @@
+package ECM2414SoftwareDevCoursework;
+
 import java.util.Random;
 import java.util.ArrayList;
 import java.util.List;
@@ -78,7 +80,7 @@ public class Bag {
                 line = reader.readLine();
             }
         }catch (IOException IOException){
-            IOException.printStackTrace();
+            System.out.println("Invalid file!");
         }
     }
 
--- PebbleGame.java
+++ PebbleGame.java
@@ -1,11 +1,13 @@
+package ECM2414SoftwareDevCoursework;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map.Entry;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.net.PasswordAuthentication;
 import java.util.Random;
 
 public class PebbleGame{
@@ -16,37 +18,31 @@ public class PebbleGame{
     private List<Thread> threadPool = new ArrayList<Thread>();
     private PebbleGame game = this;
     private volatile Player winner;
-
-    private Bag locateBag(bagName name){
-        Bag desiredBag = null;
-        for (Bag bag : this.bags){
-            if (bag.getName() == name){
-                desiredBag = bag;
-                break;
-            }
-        }
-        return desiredBag;
-    }
+    public final ReentrantLock gameLock;
 
     private void refillBag(Bag bag){
         System.out.println("refil has been called");
         for (Bag b : bags){
             System.out.println(b.getName().toString() + b.getPebbles().size());
         }
+        Bag bagA = this.bags.get(3);
+        List<Entry<Integer, bagName>> pebblesA = bagA.getPebbles();
+        Bag bagB = this.bags.get(4);
+        List<Entry<Integer, bagName>> pebblesB = bagA.getPebbles();
+        Bag bagC = this.bags.get(5);
+        List<Entry<Integer, bagName>> pebblesC = bagA.getPebbles();
+
         switch (bag.getName()){
             case X:
-                Bag bagA = locateBag(bagName.A);
-                bag.setPebbles(bagA.getPebbles());
+                bag.setPebbles(pebblesA);
                 bagA.clearPebbles();
                 break;
             case Y:
-                Bag bagB = locateBag(bagName.B);
-                bag.setPebbles(bagB.getPebbles());
+                bag.setPebbles(pebblesB);
                 bagB.clearPebbles();
                 break;
             case Z:
-                Bag bagC = locateBag(bagName.C);
-                bag.setPebbles(bagC.getPebbles());
+                bag.setPebbles(pebblesC);
                 bagC.clearPebbles();
                 break;
             case A: case B: case C: // this should never be true
@@ -68,11 +64,18 @@ public class PebbleGame{
     }
 
     public void endGame(){
-        System.out.println("The winner was: " + this.winner.playerNumber.toString());
+        System.out.println("The winner was palyer: " + this.winner.playerNumber.toString());
+        for (Player player : this.players){
+            player.closeFile();
+        }
+        System.out.println("All files are now closed.\nThank you for playing.");
         System.out.println("This is now the endgame method then the program should end.");
+        this.gameLock.unlock();
+        System.exit(0);
     }
 
     public void stop(){
+        this.gameLock.lock();
         System.out.println("Stop has been called");
         for (Thread thread : this.threadPool){
             thread.interrupt();
@@ -80,11 +83,20 @@ public class PebbleGame{
         this.endGame();
     }
 
-    public void safeQuit(){
-        for (Player player : this.players){
-            player.closeFile();
+    public Bag bagGenerator(Integer numOfPlayers, bagName bagName, Scanner sc){
+        Boolean flag = true;
+        Bag bag = new Bag(bagName);
+        while (flag){
+            System.out.println("Please enter the location of bag " + bagName.toString() + " to load: ");
+            String input = sc.nextLine();
+            bag = new Bag(bagName, input);
+            if (bag.getPebbles().size() < numOfPlayers*11){
+                System.out.println("Please enter a valid file that has at least 11 times the number of pebbles as number of players.");
+            }else{
+                break;
+            }
         }
-        System.out.println("All files are now closed.\n Thank you for playing.");
+        return bag;
     }
 
     class Player extends Thread{
@@ -126,11 +138,11 @@ public class PebbleGame{
         }
 
         public void pickAPebble(){
-            Bag randomBag = bags.get(random.nextInt(3));
+            Bag randomBag = bags.get(this.random.nextInt(3));
             randomBag.bagLock.lock();
             try{
                 if (randomBag.isEmpty()){
-                    refillBag(randomBag);
+                    game.refillBag(randomBag);
                 }
                 Entry<Integer, bagName> pebble = randomBag.getRandomPebble();
                 this.playersHand.add(pebble);
@@ -230,6 +242,7 @@ public class PebbleGame{
     public PebbleGame(){
         //setup scanner
         Scanner sc = new Scanner(System.in); 
+        gameLock = new ReentrantLock();
         //greeting
         System.out.println("Welcome to the PebbleGame!!\nYou will be asked to enter the number of players.\n"
                          + "You will then be asked for the location of three files in turn containing the comma "
@@ -239,16 +252,9 @@ public class PebbleGame{
         System.out.println("Please enter the number of players: "); 
         this.numOfPlayers = sc.nextInt();
         //create bags
-        System.out.println("Please enter the location of bag X to load: ");
-        sc.nextLine();
-        String input = sc.nextLine();
-        bags.add(new Bag(bagName.X, input));
-        System.out.println("Please enter the location of bag Y to load: "); 
-        input = sc.nextLine();
-        bags.add(new Bag(bagName.Y, input));
-        System.out.println("Please enter the location of bag Z to load: "); 
-        input = sc.nextLine();
-        bags.add(new Bag(bagName.Z, input));
+        bags.add(bagGenerator(this.numOfPlayers, bagName.X, sc));
+        bags.add(bagGenerator(this.numOfPlayers, bagName.Y, sc));
+        bags.add(bagGenerator(this.numOfPlayers, bagName.Z, sc));
 
         bags.add(new Bag(bagName.A));
         bags.add(new Bag(bagName.B));
